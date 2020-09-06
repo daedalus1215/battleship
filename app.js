e13d5034a91e2810c4fa4ee63aaf6ea14ffadb10
larry adams <ladams1776@yahoo.com>
2020-09-06 10:40:23 -0400

    Add the objects and, Add the styles and, Add the reference to the objects

---
+++ app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const userGrid = document.querySelector('.grid-user');
+    const computerGrid = document.querySelector('.grid-computer');
+    const displayGrid = document.querySelector('.grid-display');
+
+    const ships = document.querySelector('.ship');
+    const destroyer = document.querySelector('.destroyer-container');
+    const submarine = document.querySelector('.submarine-container');
+    const cruiser = document.querySelector('.cruiser-container');
+    const battleship = document.querySelector('.battleship-container');
+    const carrier = document.querySelector('.carrier-container');
+
+    const start = document.querySelector('#start');
+    const rotate = document.querySelector('#rotate');
+    const turnDisplay = document.querySelector('#whose-go');
+    const infoDisplay = document.querySelector('#info');
+
+
+    destroyer.onclickListener(() => {
+        destroyer.backgroundColor(blue);
+    });
+
+});

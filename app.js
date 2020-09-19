697e8fe19bea5a882f06ab73c5cee4ebbcb0909a
larry adams <ladams1776@yahoo.com>
2020-09-19 08:56:07 -0400

    generify the arguments

--- app.js
+++ app.js
@@ -73,21 +73,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const userSquares = createBoard(userGrid);
     const computerSquares = createBoard(computerGrid);
 
-    const generateShip = (ship, computerSquares) => {
+    const generateShip = (ship, squares) => {
         let randomDirection = Math.abs(Math.floor(Math.random() * ship.directions.length));
         let current = ship.directions[randomDirection];
 
         if (randomDirection === 0) direction = 1;
         if (randomDirection === 1) direction = 10;
 
-        let randomStart = Math.abs(Math.floor((Math.random() * computerSquares.length) - (ship.directions[0].length * direction)));
+        let randomStart = Math.abs(Math.floor((Math.random() * squares.length) - (ship.directions[0].length * direction)));
 
-        const isTaken = current.some(index => computerSquares[randomStart + index].classList.contains('taken'));
+        const isTaken = current.some(index => squares[randomStart + index].classList.contains('taken'));
         const isAtRightEdge = current.some(index => (randomStart + index) % width === width - 1);
         const isAtLeftEdge = current.some(index => (randomStart + index) % width === 0);
 
-        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => computerSquares[randomStart + index].classList.add('taken', ship.name));
-        else generateShip(ship, computerSquares);
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => squares[randomStart + index].classList.add('taken', ship.name));
+        else generateShip(ship, squares);
     };
 
     generateShip(ships[0], computerSquares);

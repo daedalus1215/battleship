d2bbc2099e812a3fea16d6be5f3d29e7901d5fa4
larry adams <ladams1776@yahoo.com>
2020-10-04 20:50:09 -0400

    auto format the server.js page

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const PORT = process.env.PORT || 4000;
-const socketio = require('socket.io');
-const { connect } = require('http2');
-
-
-const app = express();
-const server = http.createServer(app);
+const express = require('express')
+const path = require('path')
+const http = require('http')
+const PORT = process.env.PORT || 4000
+const socketio = require('socket.io')
+const app = express()
+const server = http.createServer(app)
 const io = socketio(server)
 
 // Set static folder
@@ -21,30 +18,29 @@ const connections = [null, null];
 
 io.on('connection', socket => {
     // console.log('New WS Connection');
+
     // Find an available player number
     let playerIndex = -1;
     for (const i in connections) {
         if (connections[i] === null) {
             playerIndex = i;
             break;
-
         }
     }
 
-    // tell the connecting client what player number they are 
+    // Tell the connecting client what player number they are
     socket.emit('player-number', playerIndex);
 
     console.log(`Player ${playerIndex} has connected`);
 
-    // ignore player 3
+    // Ignore player 3
     if (playerIndex === -1) return;
 
     connections[playerIndex] = false; // player is not ready
 
-    // Tell everyone what player number just connected
+    // Tell eveyone what player number just connected
     socket.broadcast.emit('player-connection', playerIndex);
 
-    // Handle disconnect
     socket.on('disconnect', () => {
         console.log('server.js ', 'disconnect socket');
         console.log(`Player ${playerIndex} disconnected`);
@@ -52,13 +48,10 @@ io.on('connection', socket => {
         // Tell everyone what player number just disconnected.
         socket.broadcast.emit('player-connection', playerIndex);
     })
-
     socket.on('player-ready', () => {
         socket.broadcast.emit('enemy-ready', playerIndex);
         connections[playerIndex] = true;
     })
-
-    // check player connections
     socket.on('check-players', () => {
         const players = [];
         for (const i in connections) {
@@ -68,18 +61,14 @@ io.on('connection', socket => {
         }
         socket.emit('check-players', players);
     });
-
     socket.on('fire', id => {
         console.log(`shot fired from ${playerIndex} `, id);
         socket.broadcast.emit('fire', id);
     });
-
     socket.on('fire-reply', square => {
         console.log('square: ', square);
         socket.broadcast.emit('fire-reply', square);
     });
-
-    // 10 minute limit per player.
     setTimeout(() => {
         connections[playerIndex] = null;
         socket.emit('timeout');

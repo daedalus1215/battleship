a911ebc8c02355abd8ef60b2cf40c7091a662133
larry adams <ladams1776@yahoo.com>
2020-10-20 20:20:08 -0400

    Fix the issue with drag/drop vertical in last few squares.

--- public/app.js
+++ public/app.js
@@ -322,7 +322,7 @@ document.addEventListener('DOMContentLoaded', () => {
      */
     const getShipNameLengthAndIntendedBowPosition = (intendedHullPos, shipNameAndBowId) => {
         const shipName = shipNameAndBowId.slice(0, -2);
-        const lengthOfShip = parseInt(shipNameAndBowId.substr(-1));
+        const lengthOfShip = parseInt(shipNameAndBowId.substr(-1)-1);
         const intendedBowPos = lengthOfShip + parseInt(intendedHullPos);
 
         return { shipName, lengthOfShip, intendedBowPos };
@@ -334,7 +334,11 @@ document.addEventListener('DOMContentLoaded', () => {
      */
     const getNotAllowedHorizontalAndVerticalLocations = (lengthOfShip) => {
         const notAllowedHorizontal = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 1, 11, 21, 31, 41, 51, 61, 71, 81, 91, 2, 22, 32, 42, 52, 62, 72, 82, 92, 3, 13, 23, 33, 43, 53, 63, 73, 83, 93]
-        const notAllowedVertical = [99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60]
+        const notAllowedVertical = [
+            99, 98, 97, 96, 95, 94, 93, 92, 91, 90,
+            89, 88, 87, 86, 85, 84, 83, 82, 81, 80,
+            79, 78, 77, 76, 75, 74, 73, 72, 71, 70,
+            69, 68, 67, 66, 65, 64, 63, 62, 61, 60]
         const newNotAllowedHorizontal = notAllowedHorizontal.splice(0, 10 * lengthOfShip);
         const newNotAllowedVertical = notAllowedVertical.splice(0, 10 * lengthOfShip);
 
